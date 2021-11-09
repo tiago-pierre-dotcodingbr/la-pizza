c21c37a7bb950cbcd069d7f0f47a3f0d62a0a8d5
Tiago Pierre <tiago_pierre@outlook.com>
2021-11-09 13:55:06 -0300

    adding scrollReveal

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+ScrollReveal({
+  origin: 'top',
+  distance: '38px',
+  duration: 1000,
+  reset: true
+}).reveal(
+  'h1.logo, #products h2.title, .card, #about h2.title, #about p, #about a, img, div'
+)
